acb43348ecac6bb8595c01661cf745c73689e17e
Nick Best <nick.best5456@gmail.com>
2020-05-07 09:55:34 -0700

    next:finishing defining schema types

--- forms/core/models.py
+++ forms/core/models.py
@@ -1,20 +1,5 @@
 from django.db import models
 
-class Profile(models.Model):
-    MALE = "Male"
-    FEMALE = "Female"
-    OTHER = "Other"
-    GENDER_CHOICES = (
-        (MALE, MALE),
-        (FEMALE, FEMALE),
-        (OTHER, OTHER),
-    )
-    name = models.CharField(blank=True, default="", max_length=255)
-    age = models.PositiveSmallIntegerField(null=True)
-    phone_number = models.CharField(max_length=20)
-    business = models.ForeignKey(Business, models.CASCADE, null=True)
-    gender = models.CharField(choices=GENDER_CHOICES, blank=True, default="")
-
 
 class Business(models.Model):
     NORTH_AMERICA = "North America"
@@ -31,10 +16,25 @@ class Business(models.Model):
     )
 
     name = models.CharField(blank=True, default="", max_length=255)
-    profile = models.ForeignKey(Profile, models.CASCADE, null=True)
-    location = models.CharField(choices=LOCATION_CHOICES, blank=True, default="")
+    location = models.CharField(choices=LOCATION_CHOICES, blank=True, default="", max_length=255)
     address_line_one = models.CharField(blank=True, default="", max_length=255)
     address_line_two = models.CharField(blank=True, default="", max_length=255)
+
+
+class Profile(models.Model):
+    MALE = "Male"
+    FEMALE = "Female"
+    OTHER = "Other"
+    GENDER_CHOICES = (
+        (MALE, MALE),
+        (FEMALE, FEMALE),
+        (OTHER, OTHER),
+    )
+    name = models.CharField(blank=True, default="", max_length=255)
+    age = models.PositiveSmallIntegerField(null=True)
+    phone_number = models.CharField(max_length=20)
+    gender = models.CharField(choices=GENDER_CHOICES, blank=True, default="", max_length=255)
+    business = models.ForeignKey(Business, models.SET_NULL, null=True)
 
 
 class Reference(models.Model):
@@ -44,8 +44,8 @@ class Reference(models.Model):
 
 
 class ProfileReview(models.Model):
-    profile = models.OnetoOneField(Profile, models.CASCADE, null=True, related_name="review")
+    profile = models.OneToOneField(Profile, models.CASCADE, null=True, related_name="review")
     comments = models.CharField(blank=True, default="", max_length=255)
-    profile_verified = models.Boolean(default=False)
-    phone_verified = models.Boolean(default=False)
+    profile_verified = models.BooleanField(default=False)
+    phone_verified = models.BooleanField(default=False)
     reviewed_at = models.DateField()
--- forms/core/schema.py
+++ forms/core/schema.py
@@ -1,4 +1,3 @@
-from django import forms
 import graphene
 from graphene_django.types import DjangoObjectType
 
@@ -6,15 +5,19 @@ import core.models
 
 
 class Profile(DjangoObjectType):
-    model = core.models.Profile
-    interfaces = (graphene.relay.Node,)
+    class Meta:
+        model = core.models.Profile
+        interfaces = (graphene.relay.Node,)
 
 
 class Business(DjangoObjectType):
-    model = core.models.Business
-    interfaces = (graphene.relay.Node,)
+    class Meta:
+        model = core.models.Business
+        interfaces = (graphene.relay.Node,)
 
 
 class Query(object):
     profile = graphene.Field(Profile, id=graphene.ID(required=False), pk=graphene.Int(required=False))
-    node = graphene.relay.Node.field()
+
+    business = graphene.Field(Business, id=graphene.ID(required=False), pk=graphene.Int(required=False))
+    node = graphene.relay.Node.Field()
