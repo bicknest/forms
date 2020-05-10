96ed567cb0adcc7f0d8f6dd2b11b14481ba9235d
Nick Best <nick.best5456@gmail.com>
2020-05-10 10:50:14 -0700

    next:add profile form

--- forms/frontend/forms-ui/src/Profile/ProfileForm.tsx
+++ forms/frontend/forms-ui/src/Profile/ProfileForm.tsx
@@ -1,11 +1,48 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { gql, useQuery, useMutation } from "@apollo/client";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 
-export default function ProfileForm() {
+const ProfileFormsQuery = gql`
+  query ProfileForms {
+    profile(pk: $pk) {
+      ...ProfileForm
+    }
+  }
+  ${ProfileForm.data}
+`;
+
+export default function ProfileForms() {
+  const { pk } = useParams();
+  const pk_num = Number(pk);
+  const { data, loading, error } = useQuery<
+    ProfileFormsQueryType,
+    ProfileFormsQueryVariables
+  >(ProfileFormsQuery, {
+    variables: {
+      pk: pk_num
+    }
+  });
+  if (error) {
+    return (
+      <Container>
+        <Typography>OH NO! ERROR! {error.message}</Typography>
+      </Container>
+    );
+  }
+  if (loading || !data) {
+    return (
+      <Container>
+        <Typography>LOADING...</Typography>
+      </Container>
+    );
+  }
+  const profile = data && data.profile;
   return (
     <Container>
-      <Typography variant="h1">Profile Form</Typography>
+      <Typography variant="h1">Profile Forms</Typography>
+      <ProfileForm profile={profile} />
     </Container>
   );
 }
--- forms/frontend/forms-ui/src/Profile/ProfileList.tsx
+++ forms/frontend/forms-ui/src/Profile/ProfileList.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import { gql, useQuery } from "@apollo/client";
+
+const ProfileListQuery = gql`
+  query allProfiles {
+    allProfiles {
+      name
+    }
+  }
+`;
 
 export default function ProfileList() {
+    const { data, loading, error } = useQuery<ProfileListQueryType>(ProfileListQuery);
+  if (error || !data) {
+    return <div>Error!</div>;
+  }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <Container>
       <Typography variant="h1">Profile List</Typography>
+      <ul>
+        {data?.allLoans?.edges?.map(n => n?.node && <Typography {...n.node.name} />)}
+      </ul>
     </Container>
   );
 }
